8fe01c7eb06454f7a7638580452f3a0e0c709ace
Degroot Ian-B55392 <B55392@freescale.com>
2017-11-02 16:58:05 -0500

    Added Person class and wrote out the high-level logic in comments.

--- secret_santa_elf.py
+++ secret_santa_elf.py
@@ -2,12 +2,37 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
+class Person:
+
+    def __init__(self, fname, lname, mail_addr):
+        self.first_name = fname
+        self.last_name = lname
+        self.email_addr = mail_addr
+        self.no_pair = []
+        self.giftee = None
+
+# Get people data from text file
+
+# Create Person object out of each data entry
+
+# Create two lists that each contain all of the people, these will act as stacks
+# One will be gifters and one will be giftees
+
+# Shuffle each stack
+
+# Use last person in both lists and give giftee to gifter, if they can't be paired then swap giftee
+
+# Add each gifter to a list
+
+# Send email to each person in the list, telling them their giftee
+
+
 from_addr = "ianpdegroot@gmail.com"
-to_addr = "ADDRESS YOU WANT TO SEND TO"
+to_addr = "Address you want to send to"
 msg = MIMEMultipart()
 msg['From'] = from_addr
 msg['To'] = to_addr
-msg['Subject'] = "SUBJECT OF THE MAIL"
+msg['Subject'] = "Secret Santa Elf Test Email"
  
 body = "YOUR MESSAGE HERE"
 msg.attach(MIMEText(body, 'plain'))
