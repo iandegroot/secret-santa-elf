da3e80a3a2dc1a1931f59d0bddb8f8dcb3db2ed5
Degroot Ian-B55392 <B55392@freescale.com>
2017-11-03 17:59:11 -0500

    Added logic to read data from input text file and convert that data to a list of Person objects, also added test_input_data.txt

--- secret_santa_elf.py
+++ secret_santa_elf.py
@@ -1,6 +1,6 @@
-import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+import smtplib, sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 class Person:
 
@@ -11,38 +11,66 @@ class Person:
         self.no_pair = []
         self.giftee = None
 
-# Get people data from text file
-
-# Create Person object out of each data entry
-
-# Create two lists that each contain all of the people, these will act as stacks
-# One will be gifters and one will be giftees
-
-# Shuffle each stack
-
-# Use last person in both lists and give giftee to gifter, if they can't be paired then swap giftee
-
-# Add each gifter to a list
-
-# Send email to each person in the list, telling them their giftee
+    def __str__(self):
+        if len(self.no_pair) == 0:
+            return "Person:\n{} {}\nGiving a gift to {}\n".format(self.first_name, self.last_name, self.giftee)
+        else:
+            return "Person:\n{} {}\nCan't give a gift to {}\nGiving a gift to {}\n".format(self.first_name, self.last_name, ", ".join([str(n) for n in self.no_pair]), self.giftee)
 
 
-from_addr = "ianpdegroot@gmail.com"
-to_addr = "Address you want to send to"
-msg = MIMEMultipart()
-msg['From'] = from_addr
-msg['To'] = to_addr
-msg['Subject'] = "Secret Santa Elf Test Email"
- 
-body = "YOUR MESSAGE HERE"
-msg.attach(MIMEText(body, 'plain'))
- 
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(from_addr, "P@ssword!")
-    text = msg.as_string()
-    server.sendmail(from_addr, to_addr, text)
-    server.quit()
-except:
-    print "Something went wrong..."
+def send_email():
+    from_addr = "ianpdegroot@gmail.com"
+    to_addr = "Address you want to send to"
+    msg = MIMEMultipart()
+    msg["From"] = from_addr
+    msg["To"] = to_addr
+    msg["Subject"] = "Secret Santa Elf Test Email"
+     
+    body = "YOUR MESSAGE HERE"
+    msg.attach(MIMEText(body, "plain"))
+     
+    try:
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server.ehlo()
+        server.login(from_addr, "P@ssword!")
+        text = msg.as_string()
+        server.sendmail(from_addr, to_addr, text)
+        server.quit()
+    except:
+        print("Something went wrong...")
+
+if __name__ == "__main__":
+    master_list = []
+    gifters = []
+    giftees = []
+    # Get people data from text file
+    with open(sys.argv[1]) as input_file:
+        for line in input_file:
+            # Parse people data
+            print(line)
+            person_data = line.split(", ")
+            names = person_data[0].split()
+            no_pairs = person_data[2:]
+
+            # Create Person object out of each data entry and store in the master list
+            master_list.append(Person(names[0], names[1], person_data[1]))
+
+            # Add people that this person cannot gift to
+            if no_pairs[0].strip() != "NA":
+                for n in no_pairs:
+                    master_list[-1].no_pair.append(n.strip())
+
+            # Push object onto temporary stacks
+            gifters.append(master_list[-1])
+            giftees.append(master_list[-1])
+
+    for p in master_list:
+        print(p)
+
+    # Shuffle each stack
+
+    # Use last person in both lists and give giftee to gifter, if they can't be paired then swap giftee
+
+    # Add each gifter to a list
+
+    # Send email to each person in the list, telling them their giftee
